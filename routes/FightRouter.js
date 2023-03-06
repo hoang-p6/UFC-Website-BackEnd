7983fca6f1522e7005123e2b237b76a19bf13764
odonovan1 <odogdonovan@gmail.com>
2023-03-06 05:58:25 -0500

    added full CRUD to fight controllers

--- routes/FightRouter.js
+++ routes/FightRouter.js
@@ -4,6 +4,9 @@ const middleware = require('../middleware')
 
 router.get('/list', controller.GetFights)
 router.get('/:fight_id', controller.GetFightById)
+router.post('/create', controller.CreateFight)
+router.put('/update/:fight_id', controller.UpdateFight)
+router.delete('/delete/:fight_id', controller.DestroyFight)
 
 
 module.exports = router
