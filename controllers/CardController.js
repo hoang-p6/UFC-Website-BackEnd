b3f8bcf8c0db4f9ea54d5eadb3506c0bf632e176
Phong Hoang <p.hoang6900@gmail.com>
2023-03-06 15:12:31 -0500

    Save for merge

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -1,14 +1,1 @@
-const { Card, Fighter, Fight } = require('../models')
 
-const GetCards = async (req, res) => {
-  try {
-    const card = await Card.findAll()
-    res.send(card)
-  } catch (error) {
-    throw error
-  }
-}
-
-module.exports = {
-  GetCards
-}
--- routes/CardRouter.js
+++ routes/CardRouter.js
@@ -1,7 +1,1 @@
-const router = require('express').Router()
-const controller = require('../controllers/cardController')
-const middleware = require('../middleware')
 
-router.get('/cards', controller.GetCards)
-
-module.exports = router
