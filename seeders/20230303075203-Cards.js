964fa47315492ab41ff0385075f82c07ad8957f5
odonovan1 <odogdonovan@gmail.com>
2023-03-07 10:41:05 -0500

    added more to seed data

--- seeders/20230303075203-Cards.js
+++ seeders/20230303075203-Cards.js
@@ -16,6 +16,28 @@ module.exports = {
           image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuU_KEwJqD23HXWfmnmC2OWbFbhDg1yguU6CLGtxLfCKetqvDet4z8srPjO84ar8SWNCI&usqp=CAU',
           createdAt: new Date(),
           updatedAt: new Date()
+        },
+        {
+          title: 'UFC Fight Night: Yan vs Dvalishvili',
+          date: '2023-03-11',
+          startTime: '3 PM EST',
+          city: 'Las Vegas',
+          country: 'United States of America',
+          arena: 'The Theater at Virgin Hotels',
+          image: 'https://whoisfighting.com/wp-content/uploads/events/yan-dvalishvili.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'UFC 286: Usman VS Edwards 3',
+          date: '2023-03-18',
+          startTime: '5 PM EST',
+          city: 'London',
+          country: 'United Kingdom',
+          arena: 'O2 Arena',
+          image: 'https://i2-prod.mirror.co.uk/incoming/article27796311.ece/ALTERNATES/s1200d/2_UFC-278-Usman-v-Edwards-2.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ]
     )
